dbac7237df7a49e2bf8f8246f15a21940e2ab8e2
tehtros <tehtros@gmail.com>
2012-11-20 16:18:45 -0500

    Fixed the permissions, and added the /signspy [on|off] command.
    Signed-off-by: tehtros <tehtros@gmail.com>

--- src/tehtros/bukkit/SignSpy/SignListener.java
+++ src/tehtros/bukkit/SignSpy/SignListener.java
@@ -24,28 +24,29 @@ public class SignListener implements Listener {
 		int locy = block.getY();
 		int locz = block.getZ();
 
-		plugin.signs++;
+		if(!sender.hasPermission("signspy.ignore")) {
+			if(!message[0].isEmpty() || !message[1].isEmpty() || !message[2].isEmpty() || !message[3].isEmpty()) {
+				plugin.signs++;
 
-		// As per Mason's request...
-		if(!message[0].isEmpty() || !message[1].isEmpty() || !message[2].isEmpty() || !message[3].isEmpty()) {
-			plugin.log.info("[SignSpy] " + name + " has placed a sign!");
-			plugin.log.info("[SignSpy] Location: x(" + locx + ") y(" + locy + ") z(" + locz + ")");
-			
-			for(int i = 0; i < message.length; i++) {
-				int o = i + 1;
-				if(!message[i].isEmpty()) {
-					plugin.log.info("[SignSpy] Line " + o + ": " + message[i]);
+				plugin.log.info("[SignSpy] " + name + " has placed a sign!");
+				plugin.log.info("[SignSpy] Location: x(" + locx + ") y(" + locy + ") z(" + locz + ")");
+
+				for(int i = 0; i < message.length; i++) {
+					int o = i + 1;
+					if(!message[i].isEmpty()) {
+						plugin.log.info("[SignSpy] Line " + o + ": " + message[i]);
+					}
 				}
-			}
-			
-			for(Player p : plugin.getServer().getOnlinePlayers()) {
-				if(p.hasPermission("signspy.spy") || p.isOp()) {
-					p.sendMessage(ChatColor.DARK_AQUA + "[SignSpy] " + ChatColor.DARK_PURPLE + name + ChatColor.GOLD + " has placed a sign!");
-					p.sendMessage(ChatColor.DARK_AQUA + "[SignSpy] " + ChatColor.GOLD + "Location: x(" + locx + ") y(" + locy + ") z(" + locz + ")");
-					for(int i = 0; i < message.length; i++) {
-						int o = i + 1;
-						if(!message[i].isEmpty()) {
-							p.sendMessage(ChatColor.DARK_AQUA + "[SignSpy] " + ChatColor.GOLD + "Line " + o + ": " + ChatColor.YELLOW + message[i]);
+
+				for(Player p : plugin.getServer().getOnlinePlayers()) {
+					if(p.hasPermission("signspy.spy") || p.isOp()) {
+						p.sendMessage(ChatColor.DARK_AQUA + "[SignSpy] " + ChatColor.DARK_PURPLE + name + ChatColor.GOLD + " has placed a sign!");
+						p.sendMessage(ChatColor.DARK_AQUA + "[SignSpy] " + ChatColor.GOLD + "Location: x(" + locx + ") y(" + locy + ") z(" + locz + ")");
+						for(int i = 0; i < message.length; i++) {
+							int o = i + 1;
+							if(!message[i].isEmpty()) {
+								p.sendMessage(ChatColor.DARK_AQUA + "[SignSpy] " + ChatColor.GOLD + "Line " + o + ": " + ChatColor.YELLOW + message[i]);
+							}
 						}
 					}
 				}
--- src/tehtros/bukkit/SignSpy/SignSpy.java
+++ src/tehtros/bukkit/SignSpy/SignSpy.java
@@ -1,8 +1,12 @@
 package tehtros.bukkit.SignSpy;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,6 +22,7 @@ public class SignSpy extends JavaPlugin {
 	PluginDescriptionFile pdf;
 	PluginManager pm;
 	public final SignListener signListener = new SignListener(this);
+	private Set<CommandSender> ignoreSet = new HashSet<CommandSender>();
 
 	@Override
 	public void onEnable() {
@@ -25,7 +30,7 @@ public class SignSpy extends JavaPlugin {
 		pm = getServer().getPluginManager();
 		pm.registerEvents(this.signListener, this);
 		log.info("[" + pdf.getName() + "] " + pdf.getName() + " v" + pdf.getVersion() + " is enabled! :D");
-		
+
 		try {
 			startMetrics();
 		} catch(IOException e) {
@@ -38,7 +43,31 @@ public class SignSpy extends JavaPlugin {
 		pdf = getDescription();
 		log.info("[" + pdf.getName() + "] " + pdf.getName() + " v" + pdf.getVersion() + " is disabled! D:");
 	}
-	
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("signspy")) {
+			if(sender.hasPermission("signspy.spy")) {
+				if(args.length != 1) {
+					sender.sendMessage("Incorrect usage. Please use /signspy [on|off] to enable or disable sign logging to your in-game chat.");
+				} else {
+					if(args[0].equalsIgnoreCase("on")) {
+						if(ignoreSet.contains(sender)) {
+							ignoreSet.remove(sender);
+							sender.sendMessage("You will now recieve SignSpy messages!");
+						}
+					} else if(args[0].equalsIgnoreCase("off")) {
+						ignoreSet.add(sender);
+						sender.sendMessage("You will no longer recieve SignSpy messages!");
+					} else {
+						sender.sendMessage("Incorrect usage. Please use /signspy [on|off] to enable or disable sign logging to your in-game chat.");
+					}
+				}
+			}
+		}
+		return false;
+	}
+
 	public void startMetrics() throws IOException {
 		Metrics mets = new Metrics(this);
 		Graph exGraph = mets.createGraph("Extra Stats");
